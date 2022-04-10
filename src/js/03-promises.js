89bdfbeed7cf29b86fac72c38bbd09acf1bac11f
Helen <dd040388kev@gmail.com>
2022-04-10 19:36:51 +0300

    add task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formSubmit = document.querySelector('.form');
+const delay = document.querySelector('input[name=delay]');
+const step = document.querySelector('input[name=step]');
+const amount = document.querySelector('input[name=amount]');
+
+formSubmit.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const delayStep = Number(step.value);
+  let amountInterval = 0;
+  let delayValue = Number(delay.value);
+  const intervalId = setInterval(() => {
+    amountInterval += 1;
+    if (amount.value < amountInterval) {
+      clearInterval(intervalId);
+      return;
+    }
+    createPromise(amountInterval, delayValue).then(onMakeFulfilled).catch(onMakeRejected);
+    delayValue += delayStep;
+  }, delayStep);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
+
+function onMakeFulfilled(result) {
+  Notify.success(result);
+}
+function onMakeRejected(error) {
+  Notify.failure(error);
 }
